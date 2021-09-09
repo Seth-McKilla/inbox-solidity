462aebaeb85bbf75c8b1514a8770a66b74bd5d04
Seth McCullough <seth@endevrs.dev>
2021-09-09 09:16:54 -0500

    Setup Rinkeby deploy script

--- deploy.js
+++ deploy.js
@@ -1,1 +1,30 @@
-// deploy code will go here
+require("dotenv").config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  process.env.MNEMONIC,
+  process.env.RINKEBY_API
+);
+
+const web3 = new Web3(provider);
+
+(async () => {
+  try {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log("Attempting to deploy from account", accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({
+        data: bytecode,
+        arguments: ["Hello from the Rinkeby network!"],
+      })
+      .send({ gas: "1000000", from: accounts[0], gasPrice: "5000000000" });
+
+    console.log("Contract deployed to", result.options.address);
+  } catch (error) {
+    console.error(error);
+  }
+})();
